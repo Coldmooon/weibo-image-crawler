d535b5c7f651abacc01aa1765a947e0b01bdcd2b
coldmooon <howtoliy@gmail.com>
2023-05-13 18:43:48 +0800

    Updated for link file

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ import argparse
 from weibo import weibo_image_download as wb
 
 parser = argparse.ArgumentParser(description='Weibo Image Crawler')
-parser.add_argument('-l', '--link', default='', type=str, metavar='WeiboLink',
+parser.add_argument('-l', '--link', default=None, type=str, metavar='WeiboLink',
                     help='A Weibo link to download images. For example, https://weibo.com/3178232834/MFStocIKp or https://weibo.com/3178232834/4899808463031949')
-
+parser.add_argument('-f', '--file', default=None, type=str, metavar='File',
+                    help='A file that contains lines of weibo links.')
 
 def main():
     args = parser.parse_args()
 
     if args.link is not None:
         wb(args.link)
+    elif args.file is not None:
+        links = []
+        with open(args.file, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if line:
+                    links.append(line)
+        for url in links:
+            wb(url)
     else:
         print("Please provide a weibo link.")
 
