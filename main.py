cab528f893b7342d5ad09888129838c824bf0f95
coldmooon <howtoliy@gmail.com>
2023-05-14 13:11:56 +0800

    Updated arguments for main()

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import argparse
 from weibo import weibo_image_download as wb
 
-parser = argparse.ArgumentParser(description='Weibo Image Crawler')
-parser.add_argument('-l', '--link', default=None, type=str, metavar='WeiboLink',
+parser = argparse.ArgumentParser(
+                    prog='Weibo Image Crawler',
+                    description='Download all the high-resolution images from weibo links.')
+parser.add_argument('-l', '--link', default=None, type=str, metavar='Link',
                     help='A Weibo link to download images. For example, https://weibo.com/3178232834/MFStocIKp or https://weibo.com/3178232834/4899808463031949')
 parser.add_argument('-f', '--file', default=None, type=str, metavar='File',
                     help='A file that contains lines of weibo links.')
