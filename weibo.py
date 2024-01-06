cc2759e3d1e4647d8131d6b14b33844848007924
Coldmooon <howtoliy@gmail.com>
2024-01-06 12:17:10 +0000

    Fixed a bug in downloading

--- weibo.py
+++ weibo.py
@@ -3,6 +3,7 @@ import re
 import requests
 from requests.adapters import HTTPAdapter
 import os
+import sys
 
 headers = {'User_Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
  'Cookie': ''}
